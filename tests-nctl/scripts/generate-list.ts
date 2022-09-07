682049e1b81e7e4f19745d31817637e480a9c13e
gyroflaw <ryo.kanazawa34@gmail.com>
2022-09-08 05:29:52 +0900

    fix: Smart contract assert & scripts

--- tests-nctl/scripts/generate-list.ts
+++ tests-nctl/scripts/generate-list.ts
@@ -8,6 +8,7 @@ import { investors as casperInvestors } from "./tiers/casper/casper.json";
 function generateList() {
   const price = "2612999994";
   let investors = bscInvestors.map((investor) => {
+    // investor amount is given in usdamount*100 since 1knft = 0.01usd
     return {
       accountHash: CLPublicKey.fromHex(investor.publicKey)
         .toAccountHashStr()
@@ -21,13 +22,21 @@ function generateList() {
     casperInvestors.map((investor) => {
       return {
         accountHash: investor.accountHash,
-        amount: BigNumber.from(price)
-          .mul(investor.amount)
+        amount: BigNumber.from(investor.amount)
+          .mul(price)
           .div(10 ** 9)
           .toString(),
       };
     })
   );
+
+  let auctionTokenAmount = BigNumber.from(0);
+  investors.forEach((i) => {
+    auctionTokenAmount = auctionTokenAmount.add(i.amount);
+  });
+
+  console.log(auctionTokenAmount.toString());
+
   fs.writeFileSync(
     path.resolve(__dirname, "./tiers/casper/converted.json"),
     JSON.stringify({ investors }),
--- tests-nctl/scripts/run-after-sale-action.ts
+++ tests-nctl/scripts/run-after-sale-action.ts
@@ -29,28 +29,35 @@ const addOrders = async () => {
   );
   const casperClient = new CasperClient(NODE_ADDRESS!);
 
-  const idoContractHash = await getAccountNamedKeyValue(
-    casperClient,
-    KEYS.publicKey,
-    `casper_ido_contract_hash`
-  );
+  const idoContractHash =
+    "hash-f86ccc2fb216ac512ddb44c42208cb86b7f75bf260f4505246def86d014a3c11";
 
   await idoContract.setContractHash(idoContractHash.slice(5));
   const orders: Map<string, BigNumberish> = new Map();
   const account =
-    "account-hash-2642243a3ca1abc6f1b5ad3c9f53114955533ffe1a9e76055d1f987370d1d8e0";
+    "account-hash-f2af240a5aa234d6e295ff65b011126dc002f655b1034f869f38b7b2ba60e450";
   const amount = parseFixed("100", 9);
-  investors.forEach((investor) => {
-    orders.set(`account-hash-${investor.accountHash}`, investor.amount);
-  });
   orders.set(account, amount);
-  const deployHash = await idoContract.addOrders(
-    KEYS,
-    orders,
-    DEFAULT_RUN_ENTRYPOINT_PAYMENT!
-  );
-  console.log({ deployHash });
+  const deployHash = await idoContract.addOrders(KEYS, orders, "1500000000");
   await getDeploy(NODE_ADDRESS!, deployHash);
+  // let i = 0,
+  //   j = 0;
+  // const SIZE = 10;
+  // for (i = 0; ; i += SIZE) {
+  //   const orders: Map<string, BigNumberish> = new Map();
+  //   for (j = 0; j < SIZE; j++) {
+  //     if (j === investors.length - 1) break;
+  //     orders.set(
+  //       `account-hash-${investors[j].accountHash}`,
+  //       investors[j].amount
+  //     );
+  //   }
+  //   const deployHash = await idoContract.addOrders(KEYS, orders, "1500000000");
+  //   console.log(`----${i / SIZE}-----`);
+  //   console.log({ nth: i / SIZE, deployHash });
+  //   await getDeploy(NODE_ADDRESS!, deployHash);
+  //   console.log(`----------------`);
+  // }
 };
 
 addOrders();
